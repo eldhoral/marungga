34b4fd3218e56f03aed03d8c2b4613288b690a42
eldhoral123 <eldho.rizcky@ddt.id>
2026-07-23 16:20:04 +0700

    chore: update base URL from marungga.me to marungga.org in sitemap and robots configuration

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/admin/', '/private/'],
     },
-    sitemap: 'https://marungga.me/sitemap.xml',
+    sitemap: 'https://marungga.org/sitemap.xml',
   }
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from 'next'
 import { createClient } from '@supabase/supabase-js'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const baseUrl = 'https://marungga.me'
+  const baseUrl = 'https://marungga.org'
   
   const sitemapData: MetadataRoute.Sitemap = [
     {
